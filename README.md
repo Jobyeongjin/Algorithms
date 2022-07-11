877497bb579b057feef4c6fd4f1384eef322ad42
byeongjin <qudwls7468@gmail.com>
2022-07-11 19:00:11 +0900

    Add print

---
+++ README.md
@@ -0,0 +1,22 @@
+# 📝 알고리즘 문제풀이
+
+- 하나씩, 차근차근 풀어보는 즐거운 알고리즘 세계
+- `파이썬` 으로 구현
+- 더 나은 방법이 있다면 `리뷰` 부탁드려요 🙇‍♂️
+
+<br />
+
+### **코드업**
+
+> - [1 ~ 8번 출력](./codeup/print.py)
+> - [9 ~ 입출력]()
+
+<br />
+
+#### 알고리즘 문제풀이 사이트
+
+- [코드업](https://codeup.kr/)
+- [백준](https://acmicpc.net/)
+- [프로그래머스](https://programmers.co.kr/)
+- [SWEA](https://swexpertacademy.main/main.do)
+- [해커랭크](https://hackerrank.com/)
---
+++ codeup/print.py
@@ -0,0 +1,32 @@
+# 1.
+# Hello
+print('Hello')
+
+# 2.
+# Hello World
+print('Hello World')
+
+# 3.
+# Hello
+# World
+print('Hello\nWorld')  # \n 은 줄바꿈
+
+# 4.
+# 'Hello'
+print("'Hello'")  # "" 안에 ''
+
+# 5.
+# "Hello World"
+print("\"Hello World\"")  # "" 안에 \" 쌍따옴표
+
+# 6.
+# "!@#$%^&*()'
+print("\"!@#$%^&*()'")
+
+# 7.
+# "C:\Download\'hello'.py"
+print("\"C:\Download\\\'hello\'.py\"")
+
+# 8.
+# print("Hello\nWorld")
+print("print(\"Hello\\nWorld\")")
