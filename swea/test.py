1f3f9a1d9081a12c304a0d38f3df26faa2540d17
byeongjin <qudwls7468@gmail.com>
2022-07-23 13:18:14 +0900

    Update sw-d2

--- swea/test.py
+++ swea/test.py
@@ -1,18 +1,6 @@
+# 파일 불러오기
 import sys
-sys.stdin = open('input.txt', 'r')
 
-t = int(input())
+sys.stdin = open("input.txt", "r")
 
-for tc in range(1, t + 1):
-    # 문자열로 받기
-    n = input()
-
-    # 마디의 최대 길이는 10이니 10까지만 반복
-    for i in range(1, 10):
-
-        # 만약 i번째 문자가 i부터 i*2번째 문자가 같다면
-        # KOREA로 예를들면 i번째가 5번째인 KOREA까지 왔다면 i*2는 10번째인 KOREAKOREA까지 확인
-        # 그래서 슬라이싱은 i부터 i*2
-        if n[:i] == n[i:i*2]:
-            print(f'#{tc} {i}')
-            break
+# 문제풀이는 여기에
