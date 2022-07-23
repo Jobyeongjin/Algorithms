9e91f7313751c2685a0dc0e157996676c572762a
byeongjin <qudwls7468@gmail.com>
2022-07-23 13:18:14 +0900

    Update sw-d2

--- swea/test.py
+++ swea/test.py
@@ -1,18 +1,32 @@
+# 파일 불러오기
 import sys
-sys.stdin = open('input.txt', 'r')
+
+sys.stdin = open("input.txt", "r")
+
+# 문제풀이는 여기에
 
 t = int(input())
 
 for tc in range(1, t + 1):
-    # 문자열로 받기
-    n = input()
+    # 학생 수 n, 학점이 궁금한 학생 번호 k
+    n, k = map(int, input().split())
+    # 학생들의 총점 리스트
+    data = []
+    # 평점
+    RANK = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
 
-    # 마디의 최대 길이는 10이니 10까지만 반복
-    for i in range(1, 10):
-
-        # 만약 i번째 문자가 i부터 i*2번째 문자가 같다면
-        # KOREA로 예를들면 i번째가 5번째인 KOREA까지 왔다면 i*2는 10번째인 KOREAKOREA까지 확인
-        # 그래서 슬라이싱은 i부터 i*2
-        if n[:i] == n[i:i*2]:
-            print(f'#{tc} {i}')
-            break
+    for i in range(n):
+        # 시험 및 과제 점수
+        a, b, c = map(int, input().split())
+        # 총점 구하기
+        sum = (a * 0.35) + (b * 0.45) + (c * 0.20)
+        data.append(sum)
+    # 구하고자 하는 학생 점수
+    score = data[k - 1]
+    # 내림차순으로 정렬
+    data.sort(reverse=True)
+    # 구하고자 하는 학생의 점수를 인덱스로 접근하고
+    # 동일한 평점을 부여받은 10명의 학생들은 나누기
+    result = data.index(score) // (n//10)
+    # 해당 랭크 출력
+    print(f'#{tc} {RANK[result]}')
