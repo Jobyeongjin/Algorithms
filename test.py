11bf6d30a838b1126f0a98ea5754495a931f66aa
byeongjin <qudwls7468@gmail.com>
2022-07-30 15:25:01 +0900

    Update bj-math

--- test.py
+++ test.py
@@ -7,17 +7,23 @@ sys.stdin = open("input.txt", "r")
 # 문제풀이는 여기에
 
 
-t = int(input())
+# a, b, v = map(int, input().split())
 
-cnt = t
-for tc in range(1, t + 1):
-    word = input()
+# day = 0
+# height = 0
+# while True:
+#     day += 1
+#     if a * day - b * (day - 1) >= v:
+#         break
 
-    for i in range(len(word) - 1):
-        if word[i] == word[i + 1]:
-            pass
-        elif word[i] in word[i + 1:]:
-            cnt -= 1
-            break
+# print(day)
 
-print(cnt)
+
+a, b, v = map(int, input().split())
+
+goal = v - b  # 올라가야 하는 높이
+going = a - b  # 하루에 올라가는 높이
+if goal % going == 0:
+    print(goal // going)
+else:
+    print(goal // going + 1)
