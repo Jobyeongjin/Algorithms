89be6c9b76d202f65ed69bafa7fcbd7487cee841
byeongjin <qudwls7468@gmail.com>
2022-07-31 21:42:03 +0900

    Update bj-math

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # 파일 불러오기
+import base64
 from calendar import c
 from os import sep
 import sys
@@ -8,7 +9,22 @@ sys.stdin = open("input.txt", "r")
 # 문제풀이는 여기에
 
 
-a = input().split(':')
+# n 킬로그램 설탕 배달
+# 3킬로 봉지, 5킬로 봉지
+# 봉지의 최소 개수, 없으면 -1
 
-print(a, type(a))
-print(a[1])
+
+t = int(input())
+
+for tc in range(t):
+    k = int(input())  # 층수
+    n = int(input())  # 호수
+
+    # people 리스트에 1 ~ n까지 수
+    people = [i for i in range(1, n + 1)]  # 0층
+
+    for _ in range(k):
+        for j in range(1, n):
+            people[j] += people[j - 1]  # 아래층 인덱스 값 더하기
+
+    print(people[-1])
