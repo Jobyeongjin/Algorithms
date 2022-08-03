34dcc10b30b4c60b4425b3b64958e4af3e5aeb75
byeongjin <qudwls7468@gmail.com>
2022-08-03 10:06:47 +0900

    Update mc-0802

--- test.py
+++ test.py
@@ -7,18 +7,34 @@ input = stdin.readline
 
 # 문제풀이는 여기에
 
-t = int(input())
+n = int(input())
 
-for tc in range(1, t + 1):
-    n = int(input())
+log_list = []
+for i in range(n):
+    name = input()
+    log_list.append(name)
 
-    word = ''
-    for i in range(1, n + 1):
-        n, m = input().split()
-        word += n * int(m)
+gom = 0
+set_ = set()
+for log in log_list:
+    if log == 'ENTER':
+        set_.clear()
+    else:
+        if log not in set_:
+            set_.add(log)
+            gom += 1
 
-    print(f'#{tc}')
+print(gom)
 
-    for i in range(0, len(word), 10):
-        # print(word[i:i + 10])
-        print(i)
+# log_list = ['입력값']
+# gom = 0
+# # 리스트에서 중복탐색을 할 때는 n만큼의 시간이 필요 💡
+# # 셋에서 중복탐색을 할 때는 1만큼의 시간이 필요 💡
+# set_ = set()
+# for log in log_list:
+#     if log == 'ENTER':
+#         set_.clear()
+#     else:
+#         if log not in set_:
+#             set_.add(log)
+#             gom += 1
