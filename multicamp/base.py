7b2d7433546cd5242da1666f4d5117a7fbb8f09f
byeongjin <qudwls7468@gmail.com>
2022-08-27 16:44:05 +0900

    Update mc-base

--- multicamp/base.py
+++ multicamp/base.py
@@ -1094,3 +1094,40 @@ for _ in range(int(input())):
                 answer += 1
 
     print(answer)
+
+# 또는
+
+dr = [-1, 0, 0, 1]
+dc = [0, -1, 1, 0]
+
+for _ in range(int(input())):
+    M, N, K = map(int, input().split())
+
+    land = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+
+    for _ in range(K):
+        x, y = map(int, input().split())
+        land[y][x] = 1
+
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if land[i][j] == 1 and visited[i][j] == 0:
+                cnt += 1
+                stack = [(i, j)]
+                visited[i][j] = 1
+
+                while stack:
+                    cur = stack.pop()
+
+                    for k in range(4):
+                        nr = cur[0] + dr[k]
+                        nc = cur[1] + dc[k]
+
+                        if -1 < nr < N and -1 < nc < M:
+                            if land[nr][nc] == 1 and visited[nr][nc] == 0:
+                                visited[nr][nc] = 1
+                                stack.append((nr, nc))
+
+    print(cnt)
--- test.py
+++ test.py
@@ -16,20 +16,37 @@ def pprint(list_):
 # 문제풀이는 여기에
 
 
-def hanoi(f, s, t, c):
-    if c == 1:
-        print(f, t)
-        return
+dr = [-1, 0, 0, 1]
+dc = [0, -1, 1, 0]
 
-    hanoi(f, t, s, c - 1)
-    print(f, t)
+for _ in range(int(input())):
+    M, N, K = map(int, input().split())
 
-    hanoi(s, f, t, c - 1)
+    land = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
 
+    for _ in range(K):
+        x, y = map(int, input().split())
+        land[y][x] = 1
 
-N = int(input())
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if land[i][j] == 1 and visited[i][j] == 0:
+                cnt += 1
+                stack = [(i, j)]
+                visited[i][j] = 1
 
-print(2**N - 1)
+                while stack:
+                    cur = stack.pop()
 
-if N <= 20:
-    hanoi(1, 2, 3, N)
+                    for k in range(4):
+                        nr = cur[0] + dr[k]
+                        nc = cur[1] + dc[k]
+
+                        if -1 < nr < N and -1 < nc < M:
+                            if land[nr][nc] == 1 and visited[nr][nc] == 0:
+                                visited[nr][nc] = 1
+                                stack.append((nr, nc))
+
+    print(cnt)
